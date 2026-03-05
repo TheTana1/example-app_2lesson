537ea9f03775e9879f5fd3af53b79e0b029cc0da
TheTana <fakinfa@rambler.ru>
2026-03-05 04:12:55 +0300

    я не понимаю

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function login()
+    {
+//        $credentials = request(['email', 'password']);
+//        if (! $token = auth()->attempt($credentials)) {
+//
+//        }
+//      //
+//        dd($user);
+    }
+}
